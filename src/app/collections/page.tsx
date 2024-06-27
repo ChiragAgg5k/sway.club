066cbd3754dc4161423365e6b5f0e1cccd3ae7c0
ChiragAgg5k <110609663+ChiragAgg5k@users.noreply.github.com>
2024-06-27 20:17:09 +0530

    Refactor removed unused imports

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,17 +1,5 @@
 import { api } from "@/trpc/server";
-import { ProductGrid } from "@/app/_components/product-grid";
 import { type Product } from "@/types";
-import { Input } from "@/app/_components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/app/_components/ui/select";
-import { FaMagnifyingGlass } from "react-icons/fa6";
-import { Slider } from "@/app/_components/ui/slider";
-import { Button } from "@/app/_components/ui/button";
 import CollectionsClient from "@/app/collections/client";
 
 export default async function CollectionsPage() {
